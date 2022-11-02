21474081ee1c98cc5acdbdd6a9797876e24214db
joel <joelimgu@gmail.com>
2022-11-02 18:06:41 +0100

    TCP communication working

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,19 +1,26 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+application {
+    mainClass.set("org.example.Main")
+}
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
+    implementation("com.google.code.gson:gson:2.10")
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
---
+++ src/main/java/org/example/model/communication/Package.java
@@ -0,0 +1,12 @@
+package org.example.model.communication;
+
+import java.io.Serializable;
+
+public class Package implements Serializable {
+
+    private String s;
+
+    public Package(String s) {
+        this.s = s;
+    }
+}
