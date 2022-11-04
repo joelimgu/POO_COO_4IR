5d246369063d35a24fd9d2cd83605d07a6912f5d
joel <joelimgu@gmail.com>
2022-11-04 18:06:28 +0100

    test structure created

---
+++ src/test/java/model/server/ServerTests.java
@@ -0,0 +1,13 @@
+package model.server;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ServerTests {
+
+    @Test
+    public void canRespond() {
+        assertEquals(2,2);
+    }
+}
