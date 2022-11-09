3af17787d5185206f9d7e69e621becec75ce822a
ErAzZeD <kilou28800@gmail.com>
2022-11-09 08:55:01 +0100

    Tentative de mise en place de JavaFX sur main project PROBLEME : A EXECUTER SUR PROCESSUER x86

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("java")
     id("application")
+    id("org.openjfx.javafxplugin") version "0.0.13"
+    id("org.beryx.jlink") version "2.25.0"
+}
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
 }
 
 group = "org.example"
@@ -15,6 +21,8 @@ repositories {
 }
 
 dependencies {
+    implementation("org.controlsfx:controlsfx:11.1.1")
+    implementation("org.kordamp.bootstrapfx:bootstrapfx-core:0.4.0")
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.9.0")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.9.0")
     implementation("com.google.code.gson:gson:2.10")
@@ -25,4 +33,6 @@ dependencies {
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
+
+
 
