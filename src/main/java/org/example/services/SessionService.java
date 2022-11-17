d4845b0fb90b493739d7e6268af196bc4b117895
asmun <asmunkanane1@gmail.com>
2022-11-17 12:04:21 +0100

    Error in testing : possible problem in singleton

--- src/main/java/org/example/services/SessionService.java
+++ src/main/java/org/example/services/SessionService.java
@@ -6,19 +6,23 @@ import org.jetbrains.annotations.NotNull;
 public class SessionService {
     private static SessionService m_instance ;
     private User m_localUser;
-    public static SessionService SessionService (){
-        if (m_instance == null){
+    public synchronized static SessionService SessionService (){
+        if (SessionService.m_instance== null){
             SessionService.m_instance = new SessionService();
+            System.out.printf("nouvelle instance");
         }
         return SessionService.m_instance;
     }
 
-
-    public User getM_localUser() {
+    public synchronized  SessionService getM_instance()
+    {
+        return SessionService.m_instance;
+    }
+    public synchronized User getM_localUser() {
         return m_localUser;
     }
 
-    public void setM_localUser(User m_localUser) {
+    public synchronized void setM_localUser(User m_localUser) {
         this.m_localUser = m_localUser;
     }
 
