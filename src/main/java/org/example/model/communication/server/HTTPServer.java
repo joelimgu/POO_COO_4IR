9f9a91d27de811abe938e9fb06b0ad17ad794320
Joelimgu <joelimgu@gmail.com>
2023-01-06 15:36:05 +0100

    HTTP Event subscription created

--- src/main/java/org/example/model/communication/server/HTTPServer.java
+++ src/main/java/org/example/model/communication/server/HTTPServer.java
@@ -2,17 +2,24 @@ package org.example.model.communication.server;
 
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpServer;
+import org.example.model.CustomObservable;
+import org.example.model.CustomObserver;
 import org.example.model.communication.server.handlers.GetPseudo;
 import org.example.model.communication.server.handlers.getUserHandler;
+import org.example.model.communication.server.httpEvents.HTTPEvent;
+import org.example.model.conversation.ConnectedUser;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 
-public class HTTPServer {
+public class HTTPServer implements CustomObservable<HTTPEvent> {
 
+    private final List<CustomObserver<HTTPEvent>> subscribers = new ArrayList<>();
     int port = 0;
     public HTTPServer(int port) throws IOException {
         this.port = port;
@@ -40,5 +47,27 @@ public class HTTPServer {
         outputStream.write(body.getBytes());
         outputStream.flush();
         outputStream.close();
+    }
+
+    public void notifyAllSubscribers(HTTPEvent c) {
+        this.subscribers.forEach((s) -> {
+            if(s==null) {
+                return;
+            }
+            s.notify(c);
+        });
+    }
+
+    @Override
+    public int subscribe(CustomObserver<HTTPEvent> o) {
+        this.subscribers.add(o);
+        return this.subscribers.size() - 1;
+    }
+
+    @Override
+    public CustomObserver<HTTPEvent> unsubscribe(int i) {
+        CustomObserver<HTTPEvent> o = this.subscribers.get(i);
+        this.subscribers.set(i, null);
+        return o;
     }
 }
