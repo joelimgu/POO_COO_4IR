97e77d78d2c4a255018959877c8b1c393b815321
Joelimgu <joelimgu@gmail.com>
2023-01-06 09:02:22 +0100

    observers working

--- src/test/java/model/server/ObserverTests.java
+++ src/test/java/model/server/ObserverTests.java
@@ -20,11 +20,18 @@ public class ObserverTests {
 
         @Override
         public CustomObserver<Integer> unsubscribe(int i) {
-            return null;
+            CustomObserver<Integer> o = this.subscribers.get(i);
+            this.subscribers.set(i,null);
+            return o;
         }
 
         public void notifySubscriptors(Integer i) {
-            this.subscribers.forEach((s) -> s.notify(i) );
+            this.subscribers.forEach((s) -> {
+                if (s == null) {
+                    return;
+                }
+                s.notify(i);
+            } );
         }
     }
 
@@ -41,6 +48,18 @@ public class ObserverTests {
         O2 o2 = new O2();
         o1.subscribe(o2);
         o1.notifySubscriptors(5);
+        assertEquals(5, o2.value);
+    }
+
+    @Test
+    public void unsubscribe() {
+        O1 o1 = new O1();
+        O2 o2 = new O2();
+        int s = o1.subscribe(o2);
+        o1.notifySubscriptors(5);
+        assertEquals(5, o2.value);
+        o1.unsubscribe(s);
+        o1.notifySubscriptors(6);
         assertEquals(5, o2.value);
     }
 }
